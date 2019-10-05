ff35e4c953960e9096248a04d5637692fedfa0b1
gb-db <barsamyang@gmail.com>
2019-10-04 23:28:32 -0400

    Changed second test error model file

--- TestGit/Models/ErrorViewModel.cs
+++ TestGit/Models/ErrorViewModel.cs
@@ -5,6 +5,8 @@ namespace TestGit.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        //Simple added comment for test
+        //Simple added comment for test
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
