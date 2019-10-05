cf88758555bc6dd1fddc74fd5b0107f52b544dc4
gb-db <barsamyang@gmail.com>
2019-10-05 00:09:46 -0400

    Added comments ErrorViewModel file MultyTestGit

--- TestGit/Models/ErrorViewModel.cs
+++ TestGit/Models/ErrorViewModel.cs
@@ -5,7 +5,9 @@ namespace TestGit.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        //Albert vonc es comment for test
         //Simple added comment for test
+        //Albert vonc es comment for test
         //Simple added comment for test
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
